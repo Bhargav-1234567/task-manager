70547753df0aba54103349d4a1924507f417d4f9
Bhargav Gadhiya <bhargavgadhiya601@gmail.com>
2025-08-23 13:06:45 +0530

    fixdeploy:netlyfy toml file

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
- output: 'standalone', // Recommended for Netlify
-  trailingSlash: true,
-};
-
-export default nextConfig;
